4d05f1d8194b318fdcba38e5665a40418705cb89
Maciej Jaskowski <maciej.jaskowski@gmail.com>
2011-06-22 08:18:33 +0200

    Issue 7: The user should be able to move a node of a graph using mouse Status: DONE

---
+++ example-scripts/graphExamples.groovy
@@ -0,0 +1,63 @@
+
+//select some important beans
+import java.awt.Color;
+import org.synesthesia.spring.SpringVertex;
+tmp = gController.select("BeanName", "DefaultServiceProxyFactory")
+tmp.addAll(gController.select("BeanName", "GenericCRUDServiceImplementation"))
+tmp.addAll(gController.select("BeanName", "DTOMapperFactory"))
+tmp.addAll(gController.select("BeanName", "TradingPartnerAggregateDTOMapper"))
+tmp.addAll(gController.select("BeanName", "DTOEntityConverter"))
+tmp.addAll(gController.select("BeanName", "GenericDTOMapper"))
+tmp.addAll(gController.select("BeanName", ".*Calc"))
+tmp.addAll(gController.select("BeanName", ".*Phase"))
+tmp.addAll(gController.select("BeanName", ".*\\.load\\..*"))
+tmp.addAll(gController.select("BeanName", "CalcActivityTestHelper"))
+tmp.addAll(gController.select("BeanName", "CalcRunner"))
+selected.addVertices(tmp);
+//[com.syncron.bpe.webservice.jpql.JPQLGenericDAO#301db5ec, Scheduler, QueryFunctionRepository, DataownerTreeProducer, DemandHistoryActivity, SCPAuthorization, ScheduledTaskHelper, ConvertAmountFunction, SchedulerFacade, DAOFactory, SynchronizationJobPrototype]
+
+//show in RED the gain we might get if we make them lazy
+Set<SpringVertex> gain = gController.getGain(tmp)
+System.out.println(gain.size())
+controller.addColor(gain, new Color(255,0,0))
+vv.repaint()
+
+
+// color all *DAOs  
+import java.awt.Color;
+cur = gController.select("BeanName", ".*DAO")
+controller.addColor(cur, new Color(0, 255, 100))
+vv.repaint()
+println cur.size()
+
+// show ancestors of DAOFactory with distances from it
+desc = gController.getAncestorsWithDistances(cur);
+controller.addColor(desc, new Color(0,255,255),15);
+vv.repaint()
+println desc.size()
+
+
+// collapse all selected vertices into one node
+import org.synesthesia.spring.SpringVertex;
+SpringVertex v = new SpringVertex();
+v.setBeanName("CurrentlySelected");
+
+import java.awt.Color;
+import org.synesthesia.spring.SpringVertex;
+Set<SpringVertex> cur = (HashSet<SpringVertex>) selected.getVertices()
+Set<SpringVertex> gain = gController.getGain(cur)
+graph = graph.substituteWith(gain, v)
+vv.getGraphLayout().setGraph(graph)
+vv.repaint()
+
+
+// collapse all .*Services to one node called Services
+import org.synesthesia.spring.SpringVertex;
+SpringVertex v = new SpringVertex();
+v.setBeanName("Services");
+
+import java.awt.Color;
+app = gController.select("BeanName", ".*Service");
+graph = graph.substituteWith(app, v);
+vv.getGraphLayout().setGraph(graph)
+vv.repaint()
--- src/main/java/org/synesthesia/view/EdgeShowingPlugin.java
+++ src/main/java/org/synesthesia/view/EdgeShowingPlugin.java
@@ -2,14 +2,15 @@ package org.synesthesia.view;
 
 
 import java.awt.Cursor;
+import java.awt.Point;
 import java.awt.event.InputEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.geom.Point2D;
 
+import org.apache.log4j.Logger;
 import org.synesthesia.ExtendedVisualizationViewer;
-import org.synesthesia.VerticesRepository;
 import org.synesthesia.events.VertexEventSource;
 import org.synesthesia.spring.SpringEdge;
 import org.synesthesia.spring.SpringVertex;
@@ -19,10 +20,17 @@ import edu.uci.ics.jung.algorithms.layout.Layout;
 import edu.uci.ics.jung.visualization.Layer;
 import edu.uci.ics.jung.visualization.VisualizationViewer;
 import edu.uci.ics.jung.visualization.control.AbstractGraphMousePlugin;
+import edu.uci.ics.jung.visualization.picking.PickedState;
 import edu.uci.ics.jung.visualization.transform.MutableTransformer;
 
 public class EdgeShowingPlugin extends AbstractGraphMousePlugin 
 	implements MouseListener, MouseMotionListener{
+
+	private int addToSelectionModifiers;
+	private float offsetx;
+	private float offsety;
+	private SpringVertex vertexHit;
+	private Logger logger = Logger.getLogger(EdgeShowingPlugin.class);
 
 	public EdgeShowingPlugin() {
 		this(InputEvent.BUTTON1_MASK, InputEvent.BUTTON1_MASK | InputEvent.SHIFT_MASK);
@@ -30,45 +38,54 @@ public class EdgeShowingPlugin extends AbstractGraphMousePlugin
 	
     public EdgeShowingPlugin(int selectionModifiers, int addToSelectionModifiers) {
         super(selectionModifiers);
-//        this.addToSelectionModifiers = addToSelectionModifiers;
+        this.addToSelectionModifiers = addToSelectionModifiers;
 //        this.lensPaintable = new LensPaintable();
         this.cursor = Cursor.getPredefinedCursor(Cursor.HAND_CURSOR);
     }
     
     public void mouseReleased(MouseEvent e) {
         VisualizationViewer vv = (VisualizationViewer)e.getSource();
-        down = null;
         vv.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+        vertexHit = null;
+        down = null;
     }
 
 	public void mousePressed(MouseEvent e) {
-		down = e.getPoint();
-
-		
 		ExtendedVisualizationViewer vv = (ExtendedVisualizationViewer) e.getSource();
 		GraphElementAccessor<SpringVertex, SpringEdge> pickSupport = vv.getPickSupport();
 		Point2D ip = e.getPoint();
 		
+		PickedState pickedVertexState = vv.getPickedVertexState();
 
 		final Layout<SpringVertex, SpringEdge> layout = vv.getGraphLayout();
-		final SpringVertex vertexHit = pickSupport.getVertex(layout, ip.getX(), ip.getY());
+		vertexHit = pickSupport.getVertex(layout, ip.getX(), ip.getY());
 		if(vertexHit != null){
-			VertexEventSource.getInstance().fireEvent(vertexHit);
+			logger .info("Hit: " + vertexHit);
+                if(pickedVertexState.isPicked(vertexHit) == false) {
+                	pickedVertexState.clear();
+                	pickedVertexState.pick(vertexHit, true);
+                }
+                // layout.getLocation applies the layout transformer so
+                // q is transformed by the layout transformer only
+                Point2D q = layout.transform(vertexHit);
+                // transform the mouse point to graph coordinate system
+                Point2D gp = vv.getRenderContext().getMultiLayerTransformer().inverseTransform(Layer.LAYOUT, ip);
+
+                offsetx = (float) (gp.getX()-q.getX());
+                offsety = (float) (gp.getY()-q.getY());
+			
 			vv.repaint();
 		}else{
-		    boolean accepted = checkModifiers(e);
-		    if(accepted) {
-		        vv.setCursor(cursor);
-		    }
+			logger .info("Hit nothing.");
+			vv.setCursor(cursor);
 		}
-//		final RenderContext<BVertex, BEdge> renderContext = vv.getRenderContext();
-		
+		down = e.getPoint();
+		if(vertexHit != null) e.consume();
 	}
 	
 	 public void mouseDragged(MouseEvent e) {
 	        VisualizationViewer vv = (VisualizationViewer)e.getSource();
-	        boolean accepted = checkModifiers(e);
-	        if(accepted) {
+	        if(vertexHit == null) {
 	            MutableTransformer modelTransformer = vv.getRenderContext().getMultiLayerTransformer().getTransformer(Layer.LAYOUT);
 	            vv.setCursor(cursor);
 	            try {
@@ -84,16 +101,45 @@ public class EdgeShowingPlugin extends AbstractGraphMousePlugin
 	                System.err.println("down = "+down+", e = "+e);
 	                throw ex;
 	            }
-	        
-	            e.consume();
-	            vv.repaint();
 	        }
+	        else
+	        if(vertexHit != null) {
+	        	Point p = e.getPoint();
+	        	Point2D graphPoint = vv.getRenderContext().getMultiLayerTransformer().inverseTransform(p);
+	        	Point2D graphDown = vv.getRenderContext().getMultiLayerTransformer().inverseTransform(down);
+	        	Layout layout = vv.getGraphLayout();
+	        	double dx = graphPoint.getX()-graphDown.getX();
+	        	double dy = graphPoint.getY()-graphDown.getY();
+	        	PickedState ps = vv.getPickedVertexState();
+
+	        	Point2D vp = (Point2D) layout.transform(vertexHit);
+	        	vp.setLocation(vp.getX()+dx, vp.getY()+dy);
+	        	layout.setLocation(vertexHit, vp);
+	        	down = p;
+	        	
+	        	
+	        } 
+	        vv.repaint();
+	        e.consume();
 	    }
 
 
 	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
+		logger.info("Mouse clicked");
 		
+		ExtendedVisualizationViewer vv = (ExtendedVisualizationViewer) e.getSource();
+		GraphElementAccessor<SpringVertex, SpringEdge> pickSupport = vv.getPickSupport();
+		Point2D ip = e.getPoint();
+		
+		PickedState pickedVertexState = vv.getPickedVertexState();
+
+		final Layout<SpringVertex, SpringEdge> layout = vv.getGraphLayout();
+		vertexHit = pickSupport.getVertex(layout, ip.getX(), ip.getY());
+		if(vertexHit != null){
+			logger.info("Hit: " + vertexHit);
+			VertexEventSource.getInstance().fireEvent(vertexHit);
+			vv.repaint();
+		}
 	}
 
 
